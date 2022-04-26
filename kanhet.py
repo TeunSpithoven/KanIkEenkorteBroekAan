3603259b531ab1d16c4cbeea5fbb4e6810dc6844
Teun Spithoven <74899257+TeunSpithoven@users.noreply.github.com>
2022-04-26 10:45:03 +0200

    Update kanhet.py

--- kanhet.py
+++ kanhet.py
@@ -1,7 +1,8 @@
 # kan ik een korte broek aan?
-
 import requests
 
 x = requests.get('https://www.kanikeenkortebroekaan.nl')
 
-print(x.text)
+y = 'meta property="og:title" content="Kan ik een korte broek aan? - nee! "' not in x.text
+
+print(y)
